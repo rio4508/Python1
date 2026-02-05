ef36cf4de4b02637f595f1a7680a34700370e1e4
rio4508 <aradhyaoprio@gmail.com>
2026-02-05 11:20:05 +0530

    Add files via upload

---
+++ Assignment.3(1).py
@@ -0,0 +1,5 @@
+import math
+
+num = int(input("Enter a number: "))
+result = math.factorial(num)
+print(f"Factorial of {num} is: {result}")
---
+++ Assignment.3(2).py
@@ -0,0 +1,9 @@
+import math
+
+num=int(input("enter a number: "))
+
+square_root=math.sqrt(num)
+logarithm = math.log(num,10)
+sine = math.sin(num)
+
+print(f"Square root: {square_root}\nLogarithm: {logarithm}\nSine: {sine}")
